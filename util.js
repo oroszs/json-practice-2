bc95a65a416af67dd6c0cad5ac0b33e55b2060e0
Sean Orosz <seanorosz@yahoo.com>
2022-04-21 18:10:19 -0400

    added thumbnail for sharing link

--- util.js
+++ util.js
@@ -1,5 +1,5 @@
 const getJSON = async () => {
-    const url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100/';
+    const url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=500/';
     const response = await fetch(url);
     return await response.json();
 }
